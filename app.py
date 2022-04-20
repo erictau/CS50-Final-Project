a91c5bd7ec5dd968117550882dccf905b51801b7
erictau <erictau04@gmail.com>
2022-04-20 15:05:57 -0700

    Updating layout.html template and setting up the 'setup' and 'transactions' html pages.

--- app.py
+++ app.py
@@ -2,11 +2,51 @@ import os
 
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
-from flask import Session
+from flask_session import Session
 from tempfile import mkdtemp
-from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import apology, login_required, lookup, usd
+from helpers import *
 
 #Configure App
 app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Custom filter
+app.jinja_env.filters["usd"] = usd
+
+#Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///budget.db")
+
+
+#Routes
+@app.route("/")
+def index():
+    """Homepage. Displays project info and financial dashboard."""
+    return render_template("index.html")
+
+
+@app.route("/setup", methods=["GET", "POST"])
+def setup():
+    """Starts a new project. Initializes budgets and project info."""
+    if request.method == "POST":
+        
+        return render_template("setup.html")
+
+    if request.method == "GET":
+
+        return render_template("setup.html")
+
+@app.route("/transactions", methods=["GET", "POST"])
+def transactions():
+    """Presents a record of all transactions."""
+    if request.method == "POST":
+        
+        return render_template("transactions.html")
+
+    if request.method == "GET":
+        
+        return render_template("transactions.html")
+
--- helpers.py
+++ helpers.py
@@ -21,20 +21,6 @@ def apology(message, code=400):
     return render_template("apology.html", top=code, bottom=escape(message)), code
 
 
-def login_required(f):
-    """
-    Decorate routes to require login.
-
-    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
-    """
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if session.get("user_id") is None:
-            return redirect("/login")
-        return f(*args, **kwargs)
-    return decorated_function
-
-
 def usd(value):
     """Format value as USD."""
     return f"${value:,.2f}"
